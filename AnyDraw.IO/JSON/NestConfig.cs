b4b1c536a4a5117b0b87ee9ecde15bdacb69db35
malw <anycad@qq.com>
2024-04-17 11:11:59 +0800

    增加排版设置窗口

---
+++ AnyDraw.IO/JSON/NestConfig.cs
@@ -0,0 +1,55 @@
+﻿using AnyCAD.IO.Drawing;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.IO;
+using System.Text.Json;
+
+namespace AnyDraw.IO
+{
+    public partial class NestConfig : ObservableObject
+    {
+        [ObservableProperty]
+        private double _BinWidth  = 1600;
+        [ObservableProperty]
+        private double _BinLength = 10000;
+        [ObservableProperty]
+        private double _CurveTolerance = 0.02;
+        [ObservableProperty]
+        private double _Spacing= 0.0;
+        [ObservableProperty]
+        private int _PopulationSize = 10;
+        [ObservableProperty]
+        private int _MutationRate  = 10;
+        [ObservableProperty]
+        private bool _UseClipper = false;
+        [ObservableProperty]
+        private bool _UseHole = false;
+        [ObservableProperty]
+        private int _IterationNum  = 100;
+        [ObservableProperty]
+        private int _RotationFactor  = 4;
+        [ObservableProperty]
+        private string _EnginePath = "";
+        public void Save(string fileName)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            var data = JsonSerializer.Serialize(this, options);
+            System.IO.File.WriteAllText(fileName, data);
+        }
+
+        static public NestConfig? Load(string fileName)
+        {
+            if(!System.IO.File.Exists(fileName))
+            {
+                return null;
+            }
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                var data = reader.ReadToEnd();
+                return JsonSerializer.Deserialize<NestConfig>(data);
+            }
+        }
+    }
+}
---
+++ AnyDraw/Nest/NestConfigView.xaml.cs
@@ -0,0 +1,93 @@
+﻿using AnyCAD.Foundation;
+using AnyDraw.IO;
+using System.Diagnostics;
+using System.Windows;
+
+namespace AnyDraw.Nest
+{
+    /// <summary>
+    /// NestConfigView.xaml 的交互逻辑
+    /// </summary>
+    public partial class NestConfigView : Window
+    {
+        Document _Document;
+        public NestConfigView(Document doc)
+        {
+            InitializeComponent();
+            Owner = System.Windows.Application.Current.MainWindow;
+
+            var config = NestConfig.Load(System.IO.Path.Combine(System.IO.Path.GetTempPath(), "anynest_config.json"));
+            if(config == null )
+            {
+                config = new NestConfig();
+            }
+
+            _Document = doc;
+            btnOK.IsEnabled = System.IO.File.Exists(config.EnginePath);
+
+            this.DataContext = config;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                FileName = "AnyNest.exe",
+                DefaultExt = ".exe",
+                Filter = "AnyNest Files (*.exe)|*.exe"
+            };
+            var result = dlg.ShowDialog();
+            if (result != true)
+                return;
+            var config = (NestConfig)DataContext;
+            config.EnginePath = dlg.FileName;
+            btnOK.IsEnabled = System.IO.File.Exists(config.EnginePath);
+        }
+
+        void Apply()
+        {
+            var config = (NestConfig)DataContext;
+            if (!System.IO.File.Exists(config.EnginePath))
+                return;
+
+            var db = new AnyCAD.IO.Drawing.DrawingDb();
+            string input = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "anynest.json");
+            db.Save(_Document, input);       
+
+            string configFile = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "anynest_config.json");
+            config.Save(configFile);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = config.EnginePath;
+            startInfo.Arguments = $"--input {input} --output {input} --config {configFile}";
+
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = false;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit();
+                int exitCode = process.ExitCode;
+            }
+
+            var output = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "anynest_result.json");
+            if (System.IO.File.Exists(output))
+            {
+                var nr = AnyDraw.IO.NestResult.Load(output);
+                nr.Apply(_Document);
+            }
+        }
+        private void Button_Click_OK(object sender, RoutedEventArgs e)
+        {            
+            this.Close();
+            Apply();
+        }
+
+        private void Button_Click_Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
